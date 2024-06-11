70dd3261bfe7ec8f2a1882e14b33c66f37fa3179
elias.oliveira <elias.oliveira@adviseu.com.br>
2024-06-11 13:39:23 -0300

    initial commit

---
+++ lib/ex_mon.ex
@@ -0,0 +1,21 @@
+defmodule ExMon do
+  alias ExMon.{Game, Player}
+  alias ExMon.Game.{Status, Actions}
+  @computer_name "Robotinic"
+
+  def create_player(name, move_avg, move_heal, move_rnd) do
+    Player.build(name, move_avg, move_heal, move_rnd)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player(:punch, :heal, :superpunch)
+    |> Game.start(player)
+
+    Status.print_round_message()
+  end
+
+  def make_move(move) do
+    Actions.fetch_move(move)
+  end
+end
